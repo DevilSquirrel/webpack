99d3b2bd1cff789c665451d1100e76b67658db4e
liuyuchuan <18611448841@163.com>
2023-09-20 18:03:46 +0800

    代码分离

---
+++ dist/another.bundle.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["another"],{
+
+/***/ "./src/demo3/another-module.js":
+/*!*************************************!*\
+  !*** ./src/demo3/another-module.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+
+
+console.log(lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Another','module','loaded!'],' '))
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["shared"], () => (__webpack_exec__("./src/demo3/another-module.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5vdGhlci5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQXNCO0FBQ3RCO0FBQ0EsWUFBWSxrREFBTSIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3QvLi9zcmMvZGVtbzMvYW5vdGhlci1tb2R1bGUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IF8gZnJvbSAnbG9kYXNoJ1xyXG5cclxuY29uc29sZS5sb2coXy5qb2luKFsnQW5vdGhlcicsJ21vZHVsZScsJ2xvYWRlZCEnXSwnICcpKVxyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config4.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports= {
+  mode:'development',
+  entry:{
+    index:{
+      import: './src/demo3/index.js',
+      dependOn: 'shared'
+    },
+    another: {
+      import: './src/demo3/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash'
+  },
+  devtool:'inline-source-map',
+  devServer:{
+    static:'./dist'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title:'Development'
+    })
+  ],
+  output:{
+    filename:'[name].bundle.js',
+    path:path.resolve(__dirname,'dist'),
+    clean:true,
+    publicPath:'/',
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/i,
+        use:['style-loader','css-loader'],
+      },
+      {
+        test:/\.(png|svg|jpg|jpeg|gif)$/i,
+        type:'asset/resource',
+      },
+      {
+        test:/\.(woff|woff2|eot|ttf|otf)$/i, 
+        type: 'asset/resource',
+      }
+    ]
+  },
+  optimization:{
+    runtimeChunk:'single'
+  }
+}
